b0b7dcd9c11fef354c56c7b1c1282d80ccbbe38e
JJ-Chang <jada.books@gmail.com>
2020-03-25 21:29:12 -0400

    create tic tac toe app

---
+++ src/index.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
+    handleClick(i){
+    }
+
+    render() {
+    //"this" refers to currentclass
+      return (
+        <button className="square" onClick={() => this.props.onClick()}
+           >
+          {this.props.value} 
+        </button>
+      );
+    }
+  }
+  
+  class Board extends React.Component {
+    handleClick(i){
+      const squares = this.state.squares.slice();//slice makes copy of array
+      squares[i] = this.state.xIsNext ? 'X' : 'O';//turenary operator; if true return X, if false return O
+      this.setState({
+       squares: squares,//old squares array set to new squares array - changes state of whole board at once
+       xIsNext: !this.state.xIsNext, //changes turn
+      });
+    }
+    
+    constructor(props){
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+        xIsNext: true,
+      };
+    }
+
+    renderSquare(i) {
+      return (<Square 
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+      />);
+    }
+  
+    render() {
+      const status = 'Next player: X';
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
